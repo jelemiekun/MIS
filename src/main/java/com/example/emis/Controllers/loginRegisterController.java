0e6ac94f563020abfea18202e45f580f8e9945c0
jelemiekun - pc <jeremieeden1946@gmail.com>
2024-04-15 01:36:53 +0800

    ui login register done

--- src/main/java/com/example/emis/Controllers/loginRegisterController.java
+++ src/main/java/com/example/emis/Controllers/loginRegisterController.java
@@ -1,16 +1,83 @@
 package com.example.emis.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 public class loginRegisterController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private PasswordField passwordFieldLoginPassword;
+
+    @FXML
+    private TextField passwordFieldRegisterConfirmNewPassword;
+
+    @FXML
+    private PasswordField passwordFieldRegisterNewPassword;
+
+    @FXML
+    private TextField passwordFieldRegisterShowNewPassword;
 
     @FXML
     private TextField textFieldLoginEmail;
 
     @FXML
+    private TextField textFieldLoginPassword;
+
+    @FXML
+    private TextField textFieldRegisterEmail;
+
+    @FXML
+    private ImageView toggleConfirmNewPassword;
+
+    @FXML
+    private ImageView toggleNewPassword;
+
+    @FXML
+    void anchorPaneRequestFocus(MouseEvent event) {
+
+    }
+
+    @FXML
     void clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void createAnAccountClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pressedEnter(KeyEvent event) {
+
+    }
+
+    @FXML
+    void registerAccountOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void termsAndConditions(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toggleLoginPassword(MouseEvent event) {
 
     }
 
